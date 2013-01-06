e38c4b8183dbc410353c16898d211f63b5bdd35c
OlegR <oleg.reshetnyak@shapeservices.com>
2013-01-06 10:18:27 +0200

    Project creation

---
+++ WordsTutorTests/WordsTutorTests.h
@@ -0,0 +1,13 @@
+//
+//  WordsTutorTests.h
+//  WordsTutorTests
+//
+//  Created by OlegR on 06.01.13.
+//  Copyright (c) 2013 OlegR. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WordsTutorTests : SenTestCase
+
+@end
---
+++ WordsTutorTests/WordsTutorTests.m
@@ -0,0 +1,32 @@
+//
+//  WordsTutorTests.m
+//  WordsTutorTests
+//
+//  Created by OlegR on 06.01.13.
+//  Copyright (c) 2013 OlegR. All rights reserved.
+//
+
+#import "WordsTutorTests.h"
+
+@implementation WordsTutorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WordsTutorTests");
+}
+
+@end
