21be06d39572f265c06b8a218407c77d26a4228e
OlegR <oleg.reshetnyak@shapeservices.com>
2013-05-12 15:49:20 +0300

    Main window controller

---
+++ wordstutorosx/wordstutorosx/WindowControllers/ORMainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ORMainWindowController.h
+//  wordstutorosx
+//
+//  Created by OlegR on 11.05.13.
+//  Copyright (c) 2013 OlegR. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ORMainWindowController : NSWindowController
+
+@end
---
+++ wordstutorosx/wordstutorosx/WindowControllers/ORMainWindowController.m
@@ -0,0 +1,34 @@
+//
+//  ORMainWindowController.m
+//  wordstutorosx
+//
+//  Created by OlegR on 11.05.13.
+//  Copyright (c) 2013 OlegR. All rights reserved.
+//
+
+#import "ORMainWindowController.h"
+
+@interface ORMainWindowController ()
+
+@end
+
+@implementation ORMainWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
