96719b63db375fcbfb5b53989e7260321c130fc5
OlegR <oleg.reshetnyak@shapeservices.com>
2013-06-15 11:01:08 +0300

    ORLearnWordsViewController.

---
+++ wordstutorosx/wordstutorosx/ViewControllers/ORLearnWordsViewController.h
@@ -0,0 +1,23 @@
+//
+//  ORLearnWordsViewController.h
+//  wordstutorosx
+//
+//  Created by OlegR on 18.05.13.
+//  Copyright (c) 2013 OlegR. All rights reserved.
+//
+
+
+/*!
+ @class         ORLearnWordsViewController
+ @abstract
+ */
+@interface ORLearnWordsViewController : NSViewController
+
+/*!
+ @method        learnWordsViewController
+ @abstract
+ @result
+ */
++ (ORLearnWordsViewController*)learnWordsViewController;
+
+@end
---
+++ wordstutorosx/wordstutorosx/ViewControllers/ORLearnWordsViewController.m
@@ -0,0 +1,21 @@
+//
+//  ORLearnWordsViewController.m
+//  wordstutorosx
+//
+//  Created by OlegR on 18.05.13.
+//  Copyright (c) 2013 OlegR. All rights reserved.
+//
+
+#import "ORLearnWordsViewController.h"
+
+
+@implementation ORLearnWordsViewController
+
+#pragma mark - 
+
++ (ORLearnWordsViewController*)learnWordsViewController
+{
+    return [[ORLearnWordsViewController alloc] initWithNibName:@"ORLearnWordsViewController" bundle:nil];
+}
+
+@end
