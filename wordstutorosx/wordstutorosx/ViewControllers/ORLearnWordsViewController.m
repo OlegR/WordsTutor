91bd591fd1c4c5e0f1e26ad105ae2322fc5089fd
OlegR <oleg.reshetnyak@shapeservices.com>
2013-06-22 19:25:19 +0300

    ORLearnWordsViewControllerState

--- wordstutorosx/wordstutorosx/ViewControllers/ORLearnWordsViewController.m
+++ wordstutorosx/wordstutorosx/ViewControllers/ORLearnWordsViewController.m
@@ -9,6 +9,13 @@
 #import "ORLearnWordsViewController.h"
 
 
+typedef NS_ENUM(NSInteger, ORLearnWordsViewControllerState)
+{
+    ORLearnWordsViewControllerStateCheckWord,
+    ORLearnWordsViewControllerStateNextWord
+};
+
+
 @implementation ORLearnWordsViewController
 
 #pragma mark - 
@@ -22,10 +29,38 @@
 
 - (IBAction)checkWordButtonAction:(id)sender
 {
+    [self setState:ORLearnWordsViewControllerStateNextWord];
 }
 
 - (IBAction)nextWordButtonAction:(id)sender
 {
+    [self setState:ORLearnWordsViewControllerStateCheckWord];
+}
+
+#pragma mark -
+
+- (void)setState:(ORLearnWordsViewControllerState)state
+{
+    BOOL checkWordButtonHidden = state != ORLearnWordsViewControllerStateCheckWord;
+    CGFloat checkWordButtonAlpha = state != ORLearnWordsViewControllerStateCheckWord ? 0.0 : 1.0;
+
+    [_checkWordButton.animator setAlphaValue:checkWordButtonAlpha];
+    [_checkWordButton setHidden:checkWordButtonHidden];
+    
+    BOOL otherControlsHidden = state != ORLearnWordsViewControllerStateNextWord;
+    CGFloat otherControlsAlpha = state != ORLearnWordsViewControllerStateNextWord ? 0.0 : 1.0;
+    
+    [_correctTranslationTextField.animator setAlphaValue:otherControlsAlpha];
+    [_correctTranslationTextField setHidden:otherControlsHidden];
+    
+    [_otherLanguagesTranslationsTextField.animator setAlphaValue:otherControlsAlpha];
+    [_otherLanguagesTranslationsTextField setHidden:otherControlsHidden];
+    
+    [_nextWordButton.animator setAlphaValue:otherControlsAlpha];
+    [_nextWordButton setHidden:otherControlsHidden];
+    
+    BOOL editable = state == ORLearnWordsViewControllerStateCheckWord;
+    [_wordEnteredToCheckTextField setEditable:editable];
 }
 
 @end
